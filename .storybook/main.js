e9574e7fc64e5225d27a07dac7b900f7089a5e03
tugudush <jerome2kph@gmail.com>
2024-08-11 08:59:39 +0800

    progress

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  //stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.@(js|jsx)'],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
